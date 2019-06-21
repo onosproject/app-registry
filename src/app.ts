4cd474bba02f7de446102ab0aa0e12f55ba892eb
Anton Lin <antonjlin@gmail.com>
2019-06-21 10:12:41 -0700

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import signale from 'signale'
+import cookieParser from 'cookie-parser'
+import cors from 'cors'
+import { config } from 'dotenv'
+import { applicationsRouter } from './routes/applications'
+import morgan from 'morgan'
+
+config()
+const PORT = process.env.PORT || 3000
+
+const app = express()
+
+app.use(
+    cors({
+        credentials: true,
+        origin: [
+            'http://localhost:3000'
+        ],
+    })
+)
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(express.static('public'))
+app.use(morgan('combined'))
+app.use('/api/applications', applicationsRouter)
+
+app.listen(PORT, () => signale.start(`App listening on port ${PORT}`))
+
---
+++ src/routes/applications.ts
@@ -0,0 +1,36 @@
+import { Router, Request, Response } from "express"
+import { IApplication } from "../types/application";
+import * as fs from 'fs'
+import path from 'path'
+
+export const applicationsRouter = Router()
+
+//Store this in memory - always static
+let APPS = JSON.parse(fs.readFileSync(path.join(__dirname, '../apps/all.json')) as unknown as string) as IApplication[]
+
+/**
+ * Returns list of applications based on query
+ */
+applicationsRouter.get("/", async (req: Request, res: Response) => {
+    try {
+        let { id, version } = req.query //pull query strings from request
+        let results = APPS
+        if (id) {
+            results = results.filter(x => x.id === id)
+            if (results.length === 0) {
+                return res.status(400).json({ error: `No applications found with id: ${id}` })
+            }
+        }
+        if (version) {
+            results = results.filter(x =>
+                x.versions && (x.versions as any)[version]
+            )
+            if (results.length === 0) {
+                return res.status(400).json({ error: `No applications found supporting version ${version}` })
+            }
+        }
+        return res.json(results)
+    } catch (err) {
+        return res.status(500).json({ error: process.env.NODE_ENV === "production" ? "INTERNAL SERVER ERROR" : err })
+    }
+})
