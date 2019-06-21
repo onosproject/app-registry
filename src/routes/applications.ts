d78dfb5cf84288c641e2292d97461b3336f704e1
Anton Lin <antonjlin@gmail.com>
2019-06-21 15:20:32 -0700

    refactoring/formatting - tslint edits

--- src/routes/applications.ts
+++ src/routes/applications.ts
@@ -1,29 +1,29 @@
-import { Router, Request, Response } from "express"
-import { IApplication } from "../types/application";
-import * as fs from 'fs'
-import path from 'path'
+import { Request, Response, Router } from "express"
+import * as fs from "fs"
+import path from "path"
+import { IApplication } from "../types/application"
 
 export const applicationsRouter = Router()
 
-//Store this in memory - always static
-let APPS = JSON.parse(fs.readFileSync(path.join(__dirname, '../apps/all.json')) as unknown as string) as IApplication[]
+// Store this in memory - always static
+const APPS = JSON.parse(fs.readFileSync(path.join(__dirname, "../apps/all.json")) as unknown as string) as IApplication[]
 
 /**
  * Returns list of applications based on query
  */
 applicationsRouter.get("/", async (req: Request, res: Response) => {
     try {
-        let { id, version } = req.query //pull query strings from request
+        const { id, version } = req.query // pull query strings from request
         let results = APPS
         if (id) {
-            results = results.filter(x => x.id === id)
+            results = results.filter((x) => x.id === id)
             if (results.length === 0) {
                 return res.status(400).json({ error: `No applications found with id: ${id}` })
             }
         }
         if (version) {
-            results = results.filter(x =>
-                x.versions && (x.versions as any)[version]
+            results = results.filter((x) =>
+                x.versions && (x.versions as any)[version],
             )
             if (results.length === 0) {
                 return res.status(400).json({ error: `No applications found supporting version ${version}` })
