4b04e19e7629b9e46dfe93f025c71738ec95d58a
Anton Lin <antonjlin@gmail.com>
2019-06-24 16:16:05 -0700

    add new apps

---
+++ src/scripts/combine.js
@@ -0,0 +1,34 @@
+
+const fs = require('fs')
+const path = require('path')
+const signale = require('signale')
+
+const testFolder = path.join(__dirname, "../jsons")
+const existingFolder = path.join(__dirname, "../apps")
+
+const allApps = []
+
+fs.readdir(testFolder, (err, files) => {
+    let passed = true
+    if (err) { throw err }
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < files.length; i++) {
+        const j = JSON.parse(fs.readFileSync(`${testFolder}/${files[i]}`, "utf8"))
+        let v = j.versions['1.15.0']
+        try{
+            const otherApp = JSON.parse(fs.readFileSync(`${existingFolder}/${files[i]}`, "utf8"))
+            console.log(otherApp)
+            otherApp.versions['1.15.0'] = v
+            fs.writeFileSync(`${existingFolder}/${files[i]}`, JSON.stringify(otherApp, null, 4))
+        }catch(err){
+            console.log(err)
+            fs.writeFileSync(`${existingFolder}/${files[i]}`, JSON.stringify(j, null, 4))
+        }
+
+    }
+    if (passed) {
+        signale.complete(`All ${files.length} files completed validation`)
+    } else {
+        signale.fatal("Files did not complete validation")
+    }
+})
