2a5996c6d25364794de39cfcf61daca4e5471404
Anton Lin <antonjlin@gmail.com>
2019-06-24 12:38:52 -0700

    Add Github autodeploy + /applications page (#8)
    * add eu.* namespace apps
    
    * fix version querying by onosVersion
    
    * add html page
    
    * Update package.json
    
    * update package/tslint
    
    * add deploy
    
    * build project
    
    * add /applications test

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,11 @@ import cors from "cors"
 import { config } from "dotenv"
 import express from "express"
 import morgan from "morgan"
+import path from "path"
 import signale from "signale"
 import { applicationsRouter } from "./routes/applications"
-
 config()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 const app = express()
 
 app.use(
@@ -21,9 +21,12 @@ app.use(
 )
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.use(express.static("public"))
 if (process.env.NODE_ENV === "production") { app.use(morgan("combined")) }
 app.use("/api/applications", applicationsRouter)
+app.get("/applications",(req,res)=> {
+    res.sendFile(path.join(__dirname, "./views/applications.html"))
+})
+app.use("/static", express.static(path.join(__dirname, "public")))
 
 app.listen(PORT, () => signale.start(`App listening on port ${PORT}`))
 export default app
---
+++ src/deploy.ts
@@ -0,0 +1,75 @@
+import bodyParser from "body-parser"
+import * as cp from "child_process"
+import express from "express"
+import shell from "shelljs"
+import signale from "signale"
+import { argv } from "yargs"
+
+const testMode = Boolean(argv.test)
+const port = process.env.DEPLOY_PORT || 8081
+
+let app: cp.ChildProcess
+
+const launchApp = () => {
+    app = cp.fork("./build/app.js", [String(testMode)])
+    signale.info("Started app")
+}
+
+const killApp = () => {
+// tslint:disable-next-line: no-unused-expression
+    app && app.kill("SIGINT")
+    signale.info("Killed app")
+}
+
+const productionServer = () => {
+// tslint:disable-next-line: no-shadowed-variable
+    const app = express()
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.post("/payload", async (req, res) => {
+        res.json({ status: "Push received" })
+        signale.info("GIT PUSH DETECTED")
+        const branch = req.body.ref
+        const changelogString = req.body.commits
+            ? req.body.commits
+                  .map(
+                      (x: any) =>
+                          `<${x.url}|${x.message}> - ${x.committer.username}`,
+                  )
+                  .join("\n")
+            : ""
+        signale.info("Git push from branch " + branch)
+
+        if (branch && branch.includes("master")) {
+            try {
+                signale.info("MASTER BRANCH DETECTED")
+                signale.await("Pulling from git...")
+                shell.exec("git pull")
+                signale.await("Installing dependencies...")
+                shell.exec("npm install")
+                signale.await("Auditing dependencies...")
+                shell.exec("npm audit fix")
+                signale.await("Building project...")
+                shell.exec("npm run build")
+                signale.success("App ready for deployment")
+                killApp()
+                launchApp()
+            } catch (err) {
+                signale.error(err)
+            }
+        }
+    })
+
+    app.listen(port, () => {
+        launchApp()
+        signale.info(`App running on port ${port}`)
+    })
+}
+
+productionServer()
+
+process.on("SIGINT", () => {
+    killApp()
+    process.exit()
+})
