df888ef889f3cc588008fc49afe72e2b3e4acf38
Anton Lin <antonjlin@gmail.com>
2019-07-18 14:02:50 -0700

    tslint fixes

--- src/public/js/applications.js
+++ src/public/js/applications.js
@@ -1,19 +1,19 @@
 // @ts-nocheck
-async function getApplications(version){
-    return (await axios.get(`/api/applications?onosVersion=${version}`))
+async function getApplications(version) {
+    return (await axios.get(`/api/applications?onosVersion=${version}`));
 }
-function buildOarHTML(versions){
-    let desiredVersion = $('#version-select').find(':selected').val()
-    let html = ''
-    for(let vkey in versions){
-        let version = versions[vkey]
-        if(version.onosVersion === desiredVersion || desiredVersion === ""){
-            html += `<a href=${version.oarURL}>v${vkey} - ONOS v${version.onosVersion}</a><br>`
+function buildOarHTML(versions) {
+    let desiredVersion = $("#version-select").find(":selected").val();
+    let html = "";
+    for (let vkey in versions) {
+        let version = versions[vkey];
+        if (version.onosVersion === desiredVersion || desiredVersion === "") {
+            html += `<a href=${version.oarURL}>v${vkey} - ONOS v${version.onosVersion}</a><br>`;
         }
     }
-    return html
+    return html;
 }
-function renderRow(app){
+function renderRow(app) {
     return (`
     <tr>
         <td><a href=${app.url}>${app.title}</a></td>
@@ -22,12 +22,12 @@ function renderRow(app){
         <td>${buildOarHTML(app.versions)}</td>
         <td>${app.id}</td>
     </tr>
-    `)
+    `);
 }
 
-async function renderTable(res){
-    
-        $('#apps-table').html(`
+async function renderTable(res) {
+
+        $("#apps-table").html(`
         <thead>
         <tr>
             <th scope="col">Title</th>
@@ -38,45 +38,43 @@ async function renderTable(res){
         </tr>
         </thead>
         ${res.data
-            .sort((a,b) => {
-                return (a.title > b.title) ? 1 : -1
+            .sort((a, b) => {
+                return (a.title > b.title) ? 1 : -1;
 
             })
             .map(x => renderRow(x))
             .join()
         }
-        `)
+        `);
 }
 
-function renderDropdown(res){
-    let allVersions = []
+function renderDropdown(res) {
+    let allVersions = [];
     res.data.forEach(app => {
         Object.values(app.versions).forEach(version => {
-            allVersions.push(version.onosVersion)
-        })
-    })
-    let menuVersions = _.uniq( allVersions).sort()
+            allVersions.push(version.onosVersion);
+        });
+    });
+    let menuVersions = _.uniq( allVersions).sort();
 
-    $('#version-select').html(() => {
-        
+    $("#version-select").html(() => {
+
         return (
             [`<option value="">*</option>`] + menuVersions.map(x => `<option value="${x}">${x}</option>`).join("\n")
-        )
-    })
+        );
+    });
 }
 
 $(document).ready(function() {
-    $('#version-select').val("")
-    getApplications($('#version-select').find(':selected').val()).then(data => {
-        renderTable(data)
-        renderDropdown(data)
-    })
+    $("#version-select").val("");
+    getApplications($("#version-select").find(":selected").val()).then(data => {
+        renderTable(data);
+        renderDropdown(data);
+    });
 
-
-    $('#version-select').on('change', () => {
-        getApplications($('#version-select').find(':selected').val()).then(data => {
-            renderTable(data)
-        })
-    })
+    $("#version-select").on("change", () => {
+        getApplications($("#version-select").find(":selected").val()).then(data => {
+            renderTable(data);
+        });
+    });
 } );
-
